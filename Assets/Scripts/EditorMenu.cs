b3689afc5d96a312b8b8275897f0ab91d215b182
AmandaCascales <acv41@gcloud.ua.es>
2026-05-20 10:10:20 +0200

    PauseMenu
    Opcional a la espera de cambiar de sistema de inputs

--- Assets/Scripts/EditorMenu.cs
+++ Assets/Scripts/EditorMenu.cs
@@ -3,7 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class EditorMenu : MonoBehaviour
 {
-
+    [SerializeField]
+    private GameObject _pausePanel;
+    void Update()
+    {
+        // hasta que cambiemos de sistema de control, mantenemos esta porqueria
+        if ((SceneManager.GetActiveScene().name == "PreparationScene" || SceneManager.GetActiveScene().name == "GameScene") && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log(_pausePanel.activeInHierarchy);
+            if(!_pausePanel.activeInHierarchy)
+                _pausePanel.SetActive(true);
+            else
+                _pausePanel.SetActive(false);
+        }
+    }
     public void OnClickButton(string sceneName)
     {
         Debug.Log(sceneName);
